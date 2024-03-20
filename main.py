15abe494cbfe711331a7367a929793edd4056dc8
Mudit Tyagi <mudittyagi@Mudits-MacBook-Air.local>
2024-03-20 18:47:52 +0530

    data validation pipeline

--- main.py
+++ main.py
@@ -10,3 +10,15 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+
+from textsummarizer.pipeline.data_validation import DataValidationTrainingPipeline
+
+try:
+   logger.info(f"\n\n>>>>>> DATA VALIDATION STARTED <<<<<<\n\n") 
+   data_validation = DataValidationTrainingPipeline()
+   data_validation.main()
+   logger.info(f"\n\n>>>>>> DATA VALIDATION COMPLETED <<<<<<\n\n")
+except Exception as e:
+        logger.exception(e)
+        raise e
